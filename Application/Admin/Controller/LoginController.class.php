5bc4a9353f18d7f150a7af82f48f949121021426
pengshizhong <523724329@qq.com>
2014-08-05 18:11:42 +0800

    数据库操作字符串加引号，回去封装，晚上搞定登录
    Signed-off-by: pengshizhong <523724329@qq.com>

--- Application/Admin/Controller/LoginController.class.php
+++ Application/Admin/Controller/LoginController.class.php
@@ -3,22 +3,36 @@ namespace Admin\Controller;
 use Think\Controller;
 class LoginController extends Controller {
     public function index(){
-    	
-  //  var_dump( $test->where('id=1')->select());
+    	session('[start]');
+        //$ctoken=cookie('token');
+if(cookie('token')&&cookie('token')==session('token')){
+    echo "已登录的账号";
+}
+else {
+    echo "不存在的token";
     $this->display();
+}
+        $this->display();
     }
     public function yanzheng(){
-        session_start();
-        if($_COOKIE['token']==$_SESSION['token'])
-            echo "两个token相同";
-        echo $_COOKIE['token'];
-            echo $_SESSION['token'];
-    
+        $user=M('User');
+        $username=I('post.username');
+        //echo $username;
+        $userid=$user->where("userid='".$username."'")->select();
+        if(!$userid)
+            echo "不存在的账户";
+
+        var_dump($userid);
+        echo $userid;
+        session('[start]');
+        $token=md5(date("Y m d h i s"));
+        session('token',$token);
+        cookie('token',$token,3600);
+    }
     		
-    		// $token=md5(date("Y m d h i s"));
-      //       $_SESSION['token']=$token;
+       // $token=md5(date("Y m d h i s"));
+       //       $_SESSION['token']=$token;
       //       setcookie('token',$token);
     	
     }
 
-}
